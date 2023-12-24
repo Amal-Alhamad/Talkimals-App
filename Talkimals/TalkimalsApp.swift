09c039872e014de9123592f3c9e8e3b748606e24
Amal-Alhamad <amalhamad1419@gmail.com>
2023-12-24 14:09:02 +0300

    Initial Commit

---
+++ Talkimals/TalkimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkimalsApp.swift
+//  Talkimals
+//
+//  Created by Amal Saad on 11/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TalkimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
